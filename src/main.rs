875ec9664ed1099792e435b4400e561bab239403
thomasbubi <4480324+thomasbubi@users.noreply.github.com>
2020-03-13 22:07:25 +0100

    get cmd arguments print animal emoji

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+    //default values for resolution, filename
+    //let mut width:u32 = 600;
+    //let mut height:u32 = 400;
+    //let mut filename ="output.png";
+
+    //get command-line arguments
+    //https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html
+    let args: Vec<String> = env::args().collect();
+
+    //print animal emoji
+    if args.contains(&"--dog".to_string()) {
+        println!("woof, woof 🐶");
+    } else {
+        println!("meow 🐱");
+    }
+
 }
